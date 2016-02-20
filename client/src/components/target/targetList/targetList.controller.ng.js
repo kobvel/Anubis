88ea5686647bb54e5686abae38bbf9c4f0983ca3
kobvel <kobvel@gmail.com>
2016-02-20 18:50:39 +0200

    initial changes for target list

--- client/src/components/target/targetList/targetList.controller.ng.js
+++ client/src/components/target/targetList/targetList.controller.ng.js
@@ -2,7 +2,7 @@ angular
     .module("anubis")
     .controller("TargetListController", TargetListController);
 
-TargetListController.$inject = ["$scope", "$meteor", "$rootScope", 'toastr', '$timeout'];
+TargetListController.$inject = ["$scope", "$meteor", "$rootScope", "toastr", "$timeout"];
 
 function TargetListController($scope, $meteor, $rootScope, toastr, $timeout) {
     var self = this;
@@ -13,7 +13,8 @@ function TargetListController($scope, $meteor, $rootScope, toastr, $timeout) {
 
     self.user = Meteor.userId();
     self.targets = $meteor.collection(Targets).subscribe('targets');
-
+    
+    
     $timeout(function () {
         self.targets.forEach(function (element) {
             console.log(element.targets);
@@ -25,5 +26,26 @@ function TargetListController($scope, $meteor, $rootScope, toastr, $timeout) {
         self.newTarget.owner = $rootScope.currentUser._id;
         self.targets.push(self.newTarget);
         self.newTarget = '';
+    }
+
+    self.countToday = function (progress, frequency, goalDate, goal) {
+        var total = getTotal(progress);
+        var rest = total;
+        // progress.forEach(function (element, index, arr) {
+
+        // }, this);
+    }
+
+    self.countTotalProgress = function (progress, goal) {
+        var total = getTotal(progress);
+        return Math.round((total / goal) * 100) / 100;
+    }
+
+    function getTotal(progress) {
+        var total = 0;
+        for (var i = 0; i < progress.length; i++) {
+            total += progress[i].value;
+        }
+        return total;
     }
 }
