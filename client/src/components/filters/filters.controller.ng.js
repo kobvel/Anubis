76c63118a6bb8afc0cd741cf16b3de60886e59d0
Yevhenii Kyselov <Yevhenii_Kyselov@epam.com>
2016-02-20 20:38:44 +0200

    sidebar updated

---
+++ client/src/components/filters/filters.controller.ng.js
@@ -0,0 +1,9 @@
+angular
+    .module("anubis")
+    .controller("FiltersController", FiltersController);
+
+FiltersController.$inject = ["$scope", "$meteor", "$rootScope", "toastr", "$timeout"];
+
+function FiltersController($scope, $meteor, $rootScope, toastr, $timeout) {
+
+}
--- client/src/components/filters/filters.directive.ng.js
+++ client/src/components/filters/filters.directive.ng.js
@@ -1,3 +1,9 @@
-/**
- * Created by Desmond on 20.02.16.
- */
+angular
+    .module("anubis")
+    .directive("filters", filtersDirective);
+
+function filtersDirective() {
+    return {
+        templateUrl: 'client/src/components/filters/filters-layout.ng.html'
+    };
+}
