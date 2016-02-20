754e0d49307951619a89137163b4d800c3fd4e27
kobvel <kobvel@gmail.com>
2016-02-20 13:19:48 +0200

    ng-toastr added

--- client/src/lib/routes.ng.js
+++ client/src/lib/routes.ng.js
@@ -3,14 +3,52 @@ angular
     .run(function () {
 
     })
-    .config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+    .config(function ($urlRouterProvider,
+        $stateProvider,
+        $locationProvider,
+        toastrConfig) {
         // $locationProvider.html5Mode(true);
         $urlRouterProvider.otherwise('/targets');
-        
+
         $stateProvider
             .state('targets', {
                 url: '/targets',
-                templateUrl: 'client/src/components/target/target-list.ng.html',
+                templateUrl: 'client/src/components/target/targetList/target-list.ng.html',
                 controller: 'TargetListController as vm'
             })
+
+
+        angular.extend(toastrConfig, {
+            allowHtml: false,
+            autoDismiss: false,
+            closeButton: false,
+            closeHtml: '<button>&times;</button>',
+            containerId: 'toast-container',
+            extendedTimeOut: 1000,
+            iconClasses: {
+                error: 'toast-error',
+                info: 'toast-info',
+                success: 'toast-success',
+                warning: 'toast-warning'
+            },
+            maxOpened: 0,
+            messageClass: 'toast-message',
+            newestOnTop: true,
+            onHidden: null,
+            onShown: null,
+            onTap: null,
+            positionClass: 'toast-top-right',
+            preventDuplicates: false,
+            preventOpenDuplicates: false,
+            progressBar: false,
+            tapToDismiss: true,
+            target: 'body',
+            templates: {
+                toast: 'directives/toast/toast.html',
+                progressbar: 'directives/progressbar/progressbar.html'
+            },
+            timeOut: 5000,
+            titleClass: 'toast-title',
+            toastClass: 'toast'
+        });
     });
