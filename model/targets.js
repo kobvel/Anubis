caa1de14f7cc886bc05f8b3c698fe0a8eed05f95
kobvel <kobvel@gmail.com>
2016-02-21 01:03:14 +0200

    permissions changed

--- model/targets.js
+++ model/targets.js
@@ -2,12 +2,10 @@ Targets = new Mongo.Collection("targets");
 
 Targets.allow({
     insert: function (userId, target) {
-        // return userId && target.owner === userId;
-        return true;
+        return target.owner === 1 || target.owner === userId;
     },
     update: function (userId, target, fields, modifier) {
-        // return userId && target.owner === userId;
-        return true;
+        return target.owner === 1 || target.owner === userId;
     },
     remove: function (userId, target) {
         return userId && target.owner === userId;
