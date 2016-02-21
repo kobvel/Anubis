82725d42731cea3d52a98a279656af1295cdee5e
TheBarbod <hero@mail.ua>
2016-02-21 04:51:41 +0200

    Graphs are now in power rangers mode

--- client/graphPlayground/d3.controller.ng.js
+++ client/graphPlayground/d3.controller.ng.js
@@ -10,129 +10,110 @@ function GraphController($scope, $meteor, $timeout, metricService) {
     self.user = Meteor.userId();
     self.targets = $meteor.collection(Targets).subscribe('targets');
 
-    $timeout(function(){
+    $timeout(function() {
         InitChart();
     }, 3000);
 
     function InitChart() {
-         nv.addGraph(function() {
-        var chart = nv.models.lineChart();
-        var fitScreen = false;
-        var width = 600;
-        var height = 300;
-        var zoom = 1;
+        nv.addGraph(function() {
+            var chart = nv.models.lineChart();
+            var fitScreen = false;
+            var width = 600;
+            var height = 300;
+            var zoom = 1;
 
-        chart.useInteractiveGuideline(true);
+            chart.useInteractiveGuideline(true);
 
-        chart.xAxis
-            .axisLabel('Time')
-             .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)); });
+            chart.xAxis
+                .axisLabel('Time')
+                .tickFormat(function(d) {
+                    return d3.time.format('%b %d')(new Date(d)); });
             // .ticks(d3.time.days, 1)
             // .tickFormat(d3.format(',.2f'));
 
 
-        chart.lines.dispatch.on("elementClick", function(evt) {
-            console.log(evt);
-        });
+            chart.lines.dispatch.on("elementClick", function(evt) {
+                console.log(evt);
+            });
 
-        chart.yAxis
-            .axisLabel('Value')
-            .tickFormat(d3.format(',.2f'));
-
-        d3.select('#chart1 svg')
-            .attr('perserveAspectRatio', 'xMinYMid')
-            .attr('width', width)
-            .attr('height', height)
-            .datum(sinAndCos());
-
-        setChartViewBox();
-        resizeChart();
-
-        nv.utils.windowResize(resizeChart);
-
-        function setChartViewBox() {
-            var w = width * zoom,
-                h = height * zoom;
-
-            chart
-                .width(w)
-                .height(h);
+            chart.yAxis
+                .axisLabel('Value')
+                .tickFormat(d3.format(',.2f'));
 
             d3.select('#chart1 svg')
-                .attr('viewBox', '0 0 ' + w + ' ' + h)
-                .transition().duration(500)
-                .call(chart);
-        }
+                .attr('perserveAspectRatio', 'xMinYMid')
+                .attr('width', width)
+                .attr('height', height)
+                .datum(sinAndCos());
 
+            setChartViewBox();
+            resizeChart();
 
+            nv.utils.windowResize(resizeChart);
 
-        function resizeChart() {
-            var container = d3.select('#chart1');
-            var svg = container.select('svg');
+            function setChartViewBox() {
+                var w = width * zoom,
+                    h = height * zoom;
 
-            if (fitScreen) {
-                // resize based on container's width AND HEIGHT
-                var windowSize = nv.utils.windowSize();
-                svg.attr("width", windowSize.width);
-                svg.attr("height", windowSize.height);
-            } else {
-                // resize based on container's width
-                var aspect = chart.width() / chart.height();
-                var targetWidth = parseInt(container.style('width'));
-                svg.attr("width", targetWidth);
-                svg.attr("height", Math.round(targetWidth / aspect));
+                chart
+                    .width(w)
+                    .height(h);
+
+                d3.select('#chart1 svg')
+                    .attr('viewBox', '0 0 ' + w + ' ' + h)
+                    .transition().duration(500)
+                    .call(chart);
             }
-        }
-        return chart;
-    });
 
-    function sinAndCos() {
-        //var data = self.targets[0].targets[5].progress;
-        //console.log(data);
+            function resizeChart() {
+                var container = d3.select('#chart1');
+                var svg = container.select('svg');
 
-        var data = [];
-        sum = 0;
-        self.targets[0].targets[5].progress.forEach(function(commit) {
-            sum += commit.value;
-            data.push({x : commit.date.getTime(), y : sum});
+                if (fitScreen) {
+                    // resize based on container's width AND HEIGHT
+                    var windowSize = nv.utils.windowSize();
+                    svg.attr("width", windowSize.width);
+                    svg.attr("height", windowSize.height);
+                } else {
+                    // resize based on container's width
+                    var aspect = chart.width() / chart.height();
+                    var targetWidth = parseInt(container.style('width'));
+                    svg.attr("width", targetWidth);
+                    svg.attr("height", Math.round(targetWidth / aspect));
+                }
+            }
+            return chart;
         });
-            
 
-        return [
-            {
-                values : data,
-                key : self.targets[0].targets[5].name,
-                color : "#4FD0D0"
-            }//,
-            // {
-            //     values : data,
-            //     key : self.targets[0].targets[5].name,
-            //     color : "#ff7foe"
-            // }
-        ];
+        function sinAndCos() {
+            var output = [];
+            self.targets[0].targets.forEach(function(target) {
+                var data = [];
+                sum = 0;
+                target.progress.forEach(function(commit) {
+                    sum += commit.value;
+                    data.push({ x: commit.date.getTime(), y: sum });
+                });
 
-        // var sin = [],
-        //     cos = [];
+                output.push({
+                    values: data,
+                    key: target.name,
+                    color: getRandomColor()
+                });
+            });
 
-        // for (var i = 0; i < 100; i++) {
-        //     sin.push({x: i, y: Math.sin(i/10) });
-        //     cos.push({x: i, y: .5 * Math.cos(i/10)});
-        // }
+            return output;
+        }
 
-        // return [
-        //     {
-        //         values: sin,
-        //         key: "Sine Wave",
-        //         color: "#ff7f0e"
-        //     },
-        //     {
-        //         values: cos,
-        //         key: "Cosine Wave",
-        //         color: "#2ca02c"
-        //     }
-        // ];
-    }
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF'.split('');
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
     }
     // InitChart();
-    
+
 }
