80755721e47cb0bda389f5f686a2386c800fe7a1
Yevhenii Kyselov <Yevhenii_Kyselov@epam.com>
2016-02-21 17:10:58 +0200

    search by text added

--- client/src/components/common/metric.service.ng.js
+++ client/src/components/common/metric.service.ng.js
@@ -4,7 +4,9 @@ angular.module('anubis')
 function metricService() {
     return {
         getMetric: getMetric,
-        target: {}
+        // TODO: move to special services
+        target: {},
+        searchQuery: ''
     };
     function getMetric(minutes) {
         var metric = 'day';
--- client/src/components/target/target.controller.ng.js
+++ client/src/components/target/target.controller.ng.js
@@ -2,10 +2,11 @@ angular
     .module('anubis')
     .controller('TargetController', TargetController);
 
-TargetController.$inject = ['$scope', '$meteor', 'toastr'];
+TargetController.$inject = ['$scope', '$meteor', 'toastr', 'metricService'];
 
-function TargetController($scope, $meteor, toastr) {
+function TargetController($scope, $meteor, toastr, metricService) {
     var vm = this;
     vm.user = Meteor.userId();
     vm.targets = $meteor.collection(Targets).subscribe('targets');
+    vm.metricService = metricService;
 }
