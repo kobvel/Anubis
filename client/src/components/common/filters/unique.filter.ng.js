5b740273d571440abbeb2b05e23aaa46be35f650
Yevhenii Kyselov <Yevhenii_Kyselov@epam.com>
2016-02-21 12:10:56 +0200

    Fix filters by metric. Make it unique

---
+++ client/src/components/common/filters/unique.filter.ng.js
@@ -0,0 +1,42 @@
+angular
+    .module('anubis')
+    .filter('unique', uniqueFilter);
+
+function uniqueFilter() {
+
+    return function(items, filterOn) {
+
+        if (filterOn === false) {
+            return items;
+        }
+
+        if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+            var newItems = [];
+
+            var extractValueToCompare = function(item) {
+                if (angular.isObject(item) && angular.isString(filterOn)) {
+                    return item[filterOn];
+                } else {
+                    return item;
+                }
+            };
+
+            angular.forEach(items, function(item) {
+                var isDuplicate = false;
+
+                for (var i = 0; i < newItems.length; i++) {
+                    if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) {
+                    newItems.push(item);
+                }
+
+            });
+            items = newItems;
+        }
+        return items;
+    };
+}
