201636e14148784fd8714a66854803256b7a8081
TheBarbod <hero@mail.ua>
2016-02-21 04:33:06 +0200

    Look at my graph. My graph is amazing.

--- client/graphPlayground/d3.controller.ng.js
+++ client/graphPlayground/d3.controller.ng.js
@@ -11,78 +11,10 @@ function GraphController($scope, $meteor, $timeout, metricService) {
     self.targets = $meteor.collection(Targets).subscribe('targets');
 
     $timeout(function(){
-        InitChart2();
+        InitChart();
     }, 3000);
 
     function InitChart() {
-        
-        var data = self.targets[0].targets[5].progress;
-        console.log(data);
-        // var data = [{
-        //     "value": "202",
-        //     "year": "2000"
-        // }, {
-        //     "value": "215",
-        //     "year": "2002"
-        // }, {
-        //     "value": "179",
-        //     "year": "2004"
-        // }, {
-        //     "value": "199",
-        //     "year": "2006"
-        // }, {
-        //     "value": "134",
-        //     "year": "2008"
-        // }, {
-        //     "value": "176",
-        //     "year": "2010"
-        // }];
-        var vis = d3.select("#visualisation"),
-            WIDTH = 500,
-            HEIGHT = 500,
-            MARGINS = {
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: 50
-            },
-            xScale = d3.time.scale().range([MARGINS.left, WIDTH - MARGINS.right]).domain([new Date(2016,2,20), new Date(2016,4,24)]),
-            yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0, 4]),
-            xAxis = d3.svg.axis()
-            .scale(xScale),
-            yAxis = d3.svg.axis()
-            .scale(yScale)
-            .orient("left");
-
-        vis.append("svg:g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
-            .call(xAxis);
-        vis.append("svg:g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(" + (MARGINS.left) + ",0)")
-            .call(yAxis);
-        var lineGen = d3.svg.line()
-            .x(function(d) {
-                return xScale(d.date);
-            })
-            .y(function(d) {
-                return yScale(d.value);
-            })
-            .interpolate("basis");
-        vis.append('svg:path')
-            .attr('d', lineGen(data))
-            .attr('stroke', 'green')
-            .attr('stroke-width', 2)
-            .attr('fill', 'none');
-        /*  vis.append('svg:path')
-              .attr('d', lineGen(data2))
-              .attr('stroke', 'blue')
-              .attr('stroke-width', 2)
-              .attr('fill', 'none');*/
-    }
-    
-    function InitChart2() {
          nv.addGraph(function() {
         var chart = nv.models.lineChart();
         var fitScreen = false;
@@ -94,7 +26,9 @@ function GraphController($scope, $meteor, $timeout, metricService) {
 
         chart.xAxis
             .axisLabel('Time')
-            .tickFormat(d3.format(',.2f'));
+             .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)); });
+            // .ticks(d3.time.days, 1)
+            // .tickFormat(d3.format(',.2f'));
 
 
         chart.lines.dispatch.on("elementClick", function(evt) {
@@ -153,26 +87,50 @@ function GraphController($scope, $meteor, $timeout, metricService) {
     });
 
     function sinAndCos() {
-        var sin = [],
-            cos = [];
+        //var data = self.targets[0].targets[5].progress;
+        //console.log(data);
 
-        for (var i = 0; i < 100; i++) {
-            sin.push({x: i, y: Math.sin(i/10) });
-            cos.push({x: i, y: .5 * Math.cos(i/10)});
-        }
+        var data = [];
+        sum = 0;
+        self.targets[0].targets[5].progress.forEach(function(commit) {
+            sum += commit.value;
+            data.push({x : commit.date.getTime(), y : sum});
+        });
+            
 
         return [
             {
-                values: sin,
-                key: "Sine Wave",
-                color: "#ff7f0e"
-            },
-            {
-                values: cos,
-                key: "Cosine Wave",
-                color: "#2ca02c"
-            }
+                values : data,
+                key : self.targets[0].targets[5].name,
+                color : "#4FD0D0"
+            }//,
+            // {
+            //     values : data,
+            //     key : self.targets[0].targets[5].name,
+            //     color : "#ff7foe"
+            // }
         ];
+
+        // var sin = [],
+        //     cos = [];
+
+        // for (var i = 0; i < 100; i++) {
+        //     sin.push({x: i, y: Math.sin(i/10) });
+        //     cos.push({x: i, y: .5 * Math.cos(i/10)});
+        // }
+
+        // return [
+        //     {
+        //         values: sin,
+        //         key: "Sine Wave",
+        //         color: "#ff7f0e"
+        //     },
+        //     {
+        //         values: cos,
+        //         key: "Cosine Wave",
+        //         color: "#2ca02c"
+        //     }
+        // ];
     }
     }
     // InitChart();
