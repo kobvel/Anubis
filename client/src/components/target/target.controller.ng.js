2f23ed4f86c9bc7be8e83f52dc0a0f4f3f02e054
Yevhenii Kyselov <Yevhenii_Kyselov@epam.com>
2016-02-21 04:27:27 +0200

    task adding implemented

--- client/src/components/target/target.controller.ng.js
+++ client/src/components/target/target.controller.ng.js
@@ -7,9 +7,40 @@ TargetController.$inject = ['$scope', '$meteor', 'toastr'];
 function TargetController($scope, $meteor, toastr) {
     var vm = this;
 
+    Object.assign(vm, {
+        frequencies: [{
+            name: 'hour',
+            value: 60
+        }, {
+            name: 'day',
+            value: 1440
+        }, {
+            name: 'week',
+            value: 10080
+        }, {
+            name: 'month',
+            value: 302400
+        }],
+
+        addTarget: addTarget
+    });
+
     toastr.success('Hello world!', 'Toastr fun!');
 
     vm.user = Meteor.userId();
     vm.targets = $meteor.collection(Targets).subscribe('targets');
 
+    function addTarget() {
+        vm.targets[0].targets.push({
+            frequency: vm.frequency.value,
+            goalDate: vm.goalDate,
+            goalValue: vm.goalValue,
+            metric: vm.metric,
+            metricShort: vm.metric.charAt(0),
+            name: vm.taskName,
+            progress: [],
+            startDate: (new Date()).toString(),
+            status: 'active'
+        });
+    }
 }
